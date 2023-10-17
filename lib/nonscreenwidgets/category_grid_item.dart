d7a2573bd88a1bc82d0ae7f3d7a121892233aed2
James Apps <james.apps@energise.energy>
2023-10-17 15:42:56 +0100

    used InkWell to manage tappable widgets

--- lib/nonscreenwidgets/category_grid_item.dart
+++ lib/nonscreenwidgets/category_grid_item.dart
@@ -12,24 +12,34 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          // starting colour
-          category.color.withOpacity(0.55),
-          // target colour
-          category.color.withOpacity(0.9),
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
+    // make Container tappable; alternative to GestureDetector (which does not support feedback)
+    return InkWell(
+      onTap: () {},
+      // applies only to the tapped (held down) state; the original widget is not altered
+      borderRadius: BorderRadius.circular(16),
+      // colour rendered when tapped
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          // rendered when not tapped
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [
+                // starting colour
+                category.color.withOpacity(0.55),
+                // target colour
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       ),
     );
   }
