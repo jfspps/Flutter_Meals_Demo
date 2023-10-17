2ecb534ba24d257ca76512c11e580e40c834f4be
James Apps <james.apps@energise.energy>
2023-10-17 16:18:10 +0100

    impl meals screen with update to dummy_data.dart to inc. example meals

---
+++ lib/screenwidgets/meals.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../model/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget pageContent = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (context, index) => Text(meals[index].title),
+    );
+
+    if (meals.isEmpty) {
+      pageContent = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Nothing here...',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Select a different category',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: pageContent,
+    );
+  }
+}
