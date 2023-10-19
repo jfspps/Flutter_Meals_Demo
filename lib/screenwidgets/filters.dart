54ac5551806596125cc73be2d3e82c7b9bc63215
James Apps <james.apps@energise.energy>
2023-10-19 15:21:59 +0100

    set up a FiltersScreen scaffold based widget, wip

---
+++ lib/screenwidgets/filters.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FiltersScreenState();
+  }
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFreeSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Filters'),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutenFreeSet,
+            // SwitchListTile expects a boolean
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeSet = isChecked;
+              });
+            },
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'List Gluten free meals only',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          )
+        ],
+      ),
+    );
+  }
+}
